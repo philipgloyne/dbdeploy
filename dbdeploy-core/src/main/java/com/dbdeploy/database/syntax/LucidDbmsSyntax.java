0ab34da0bac2174f56b7296afefe175b32a3455d
wiley <rkeepence@wiley.com>
2009-09-11 10:17:07 +0100

    fix sybase syntax and add lucid db syntax

---
+++ dbdeploy-core/src/main/java/com/dbdeploy/database/syntax/LucidDbmsSyntax.java
@@ -0,0 +1,26 @@
+package com.dbdeploy.database.syntax;
+
+public class LucidDbmsSyntax extends DbmsSyntax {
+
+	public String generateTimestamp() {
+		return "CURRENT_TIMESTAMP";
+	}
+
+	public String generateUser() {
+		return "CURRENT_USER";
+	}
+	
+	public String generateCommit() {
+		return "";
+	}
+	
+	public String generateBeginTransaction() {
+		return "";
+	}
+	
+	@Override
+	public String generateStatementDelimiter() {
+		return "";
+	}
+
+}
